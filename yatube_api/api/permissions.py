4a2594fb5a420e14b1c35e1ce2a0362fc3619091
Umanets Liliya <umanets.lilia@gmail.com>
2022-12-25 16:56:23 +0300

    a

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -3,9 +3,9 @@ from rest_framework import permissions
 
 class IsAuthorOrReadOnly(permissions.IsAuthenticatedOrReadOnly):
     def has_permission(self, request, view):
-        if request.method not in permissions.SAFE_METHODS:
-            return request.user.is_authenticated
-        return True
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
